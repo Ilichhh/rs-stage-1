b8a88d86b11d2e17c3ab6bed575af24cc818f2d6
ilya <shakurov.ia@gmail.com>
2022-10-03 17:14:27 +0300

    feat: finish adaptive layout, create hamburger menu

--- online-zoo/js/script.js
+++ online-zoo/js/script.js
@@ -3,16 +3,25 @@ window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu');
   const menuItem = document.querySelectorAll('.menu__item');
   const hamburger = document.querySelector('.hamburger');
+  const blocker = document.querySelector('.bg');
 
   hamburger.addEventListener('click', () => {
       hamburger.classList.toggle('hamburger_active');
       nav.classList.toggle('nav_active');
+      blocker.classList.toggle('bg_active');
   });
 
   menuItem.forEach(item => {
       item.addEventListener('click', () => {
           hamburger.classList.toggle('hamburger_active');
           nav.classList.toggle('nav_active');
+          blocker.classList.toggle('bg_active');
       })
   })
+
+  blocker.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    nav.classList.toggle('nav_active');
+    blocker.classList.toggle('bg_active');
+});
 })
