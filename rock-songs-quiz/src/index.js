80e2884d20cbf4d28f066cd14c14c458d61c4931
ilya <shakurov.ia@gmail.com>
2022-11-07 17:32:14 +0300

    init: start rock-songs-quiz project

---
+++ rock-songs-quiz/src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable arrow-parens */
+import './index.html';
+import './index.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('hello');
+});
