7aa45ae3bb2619db73c3423efcaedf596d143d40
ilya <shakurov.ia@gmail.com>
2022-12-09 15:59:20 +0300

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'c7157de2efaa49a9ac85a89b0354810c',
         });
     }
 }
