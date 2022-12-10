a8247ed06bc660f49ec598daac4f0c3197f85906
ilya <shakurov.ia@gmail.com>
2022-12-10 19:57:50 +0300

    feat: migrate news.js to ts

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -1,37 +1,39 @@
 import './news.css';
+import type { Article } from './../../../types/types';
 
 class News {
-    draw(data) {
+    draw(data: Article[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsItemTemp = <HTMLTemplateElement>document.querySelector('#newsItemTemp');
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+            const newsClone = <HTMLElement>newsItemTemp.content.cloneNode(true);
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
+            const newsPhoto = <HTMLElement>newsClone.querySelector('.news__meta-photo');
+            newsPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+            const newsAuthor = <HTMLElement>newsClone.querySelector('.news__meta-author');
+            newsAuthor.textContent = item.author || item.source.name;
+            const newsDate = <HTMLElement>newsClone.querySelector('.news__meta-date');
+            newsDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
 
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            const newsDescrTitle = <HTMLElement>newsClone.querySelector('.news__description-title');
+            newsDescrTitle.textContent = item.title;
+            const newsDescrSource = <HTMLElement>newsClone.querySelector('.news__description-source')
+            newsDescrSource.textContent = item.source.name;
+            const newsDescrContent = <HTMLElement>newsClone.querySelector('.news__description-content');
+            newsDescrContent.textContent = item.description;
+            newsClone.querySelector('.news__read-more a')?.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        const newsItem = <HTMLElement>document.querySelector('.news');
+        newsItem.innerHTML = '';
+        newsItem.appendChild(fragment);
     }
 }
 
--- migration-newip-to-ts/src/types/types.ts
+++ migration-newip-to-ts/src/types/types.ts
@@ -2,3 +2,15 @@ export interface Source {
     id: string;
     name: string;
 }
+
+export interface Article {
+    urlToImage: string;
+    author: string;
+    source: {
+        name: string;
+    };
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+}
