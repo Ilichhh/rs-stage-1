032d6b34ae3844536f0b3dd50e592d7826f3eb68
ilya <shakurov.ia@gmail.com>
2022-12-11 16:04:36 +0300

    feat: migrate app.js to ts

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -1,7 +1,11 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import type { SourceDate, ArticlesDate } from './../../types/types';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -10,8 +14,10 @@ class App {
     start() {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            ?.addEventListener('click', (e) =>
+                this.controller.getNews(e, (data: ArticlesDate) => this.view.drawNews(data))
+            );
+        this.controller.getSources((data: SourceDate) => this.view.drawSources(data));
     }
 }
 
